13781bcb5a0623cac00017b2b63bd86bb6ece86e
Marco Tuzza <marcotuzza@hotmail.it>
2024-11-04 17:52:01 +0400

    Refactor header and footer components for improved styling and layout; update home component with new content and styles

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../header/header.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
