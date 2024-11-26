07b49e31f0035e4efe3d6519aff1a1e3fbaefa54
Marco Tuzza <marcotuzza@hotmail.it>
2024-11-26 20:04:24 +0400

    Refactor visual effects implementation; create standalone EffectsComponent and remove inline styles

---
+++ src/app/effects/effects.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-effects',
+  standalone: true,
+  imports: [],
+  templateUrl: './effects.component.html',
+  styleUrl: './effects.component.scss'
+})
+export class EffectsComponent {
+
+}
