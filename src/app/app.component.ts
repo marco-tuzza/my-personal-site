76423fd3786506176255ab0f7c1be0e2c9ff423d
Marco Tuzza <marcotuzza@hotmail.it>
2024-12-07 18:52:15 +0400

    Remove footer and header components; update button border style and clear routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderMainComponent } from './header-main/header-main.component';
-import { FooterComponent } from './footer/footer.component';
 import { EffectsComponent } from './effects/effects.component';
-import { HeaderMobileComponent } from './header-mobile/header-mobile.component';
-import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet,
-    HeaderMainComponent,
-    FooterComponent,
     EffectsComponent,
-    HeaderMobileComponent,
-    NgIf,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title: string = 'my-personal-site';
-  isMobile: boolean = false;
-
-  getIsMobile(): boolean {
-    const width = document.documentElement.clientWidth;
-    const breakpoint = 425;
-
-    if (width < breakpoint) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  ngOnInit(): void {
-    this.isMobile = this.getIsMobile();
-    window.onresize = () => {
-      this.isMobile = this.getIsMobile();
-    };
-  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,3 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
+import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'contact', component: ContactComponent },
-];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-})
-export class AppRoutingModule {}
+export const routes: Routes = [];
